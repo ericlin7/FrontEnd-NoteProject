0864a3a023fb47b2e7e831b16b5aa03a418ebe05
Eric Lin <ericlin@v1044-wn-rt-a-141-19.campus-dynamic.uwaterloo.ca>
2022-09-17 19:40:48 -0400

    header is working

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h1>Note Taking Project</h1>
+        <h5>A new way of studying!</h5>
+      </div>
+    </header>
+  )
+}
+
+export default Header
