a12b619437ff734b62095cf1f2c8847fccfe1396
Eric Lin <ericlin@v1044-wn-rt-a-141-19.campus-dynamic.uwaterloo.ca>
2022-09-18 00:38:46 -0400

    image is loading

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import './Header.css'
-import 'animate.css'
+import React from "react";
+import "./Header.css";
+import "animate.css";
+import logo from "../../quizzify_logo.png";
 
 const Header = () => {
+  const data = [{ image: logo }];
+
   return (
     <header>
-      <div className="container header_container">
-        <h1 className="animate__animated animate__bounceInDown">Quizzify</h1>
-        <h5>We believe you can learn anything, <br></br> 
-            so lets remove the card making, and get to the learning.
+      <div className="container   header_container">
+        <h1 className="animate__animated animate__bounceInDown">
+          {data.map(({ image }) => {
+            return (
+              <div className="item-images">
+                <img src={image} />
+              </div>
+            );
+          })}
+        </h1>
+        <h5>
+          We believe you can learn anything, <br></br>
+          so lets remove the card making, and get to the learning.
         </h5>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
